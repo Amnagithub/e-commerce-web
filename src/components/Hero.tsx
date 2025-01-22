dabb1da68c5d7a85f1eb47686ebbfc019b692129
Amnagithub <amnaaplus1@gmail.com>
2025-01-22 09:09:29 +0500

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,35 +1,38 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
 const Hero = () => {
   return (
-    <div>
-         <div className="tracking-wide bg-posterbg mx-auto p-4">
-                <header className=" text-shadow-2 mx-auto text-center flex justify-center items-center space-y-6 ">
-                  <h1 className="font-normal text-start px-3 font-poppins mt-20  text-gray-900 text-4xl md:text-6xl lg:text-8xl ">
-                    Rocket single seater
-                    <div>
-                      <Link
-                        href="/shop"
-                        className="font-poppins text-xl text-center underline underline-offset-8 decoration-slate-800"
-                      >
-                        Shop Now
-                      </Link>
-                    </div>
-                  </h1>
-                  <Image
-                    height="150"
-                    width="150"
-                    alt="Rocket Sofa"
-                    layout="responsive"
-                    src="/pic/Rocket.png"
-                    className="mt-4 lg:mt-0 lg:max-w-[50%]"
-                  />
-                </header>
-              </div>
-        
-    </div>
-  )
-}
+    <div className="bg-[#FFEBB8] min-h-[900] px-4 sm:px-8 lg:px-20">
+      <div className="container mx-auto grid grid-cols-2 items-center">
+        {/* Text Section */}
+        <div className="text-left space-y-4">
+          <h1 className="font-poppins font-semibold text-gray-900 text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-tight">
+            Rocket single<br />seater
+          </h1>
+         
+          <Link
+            href="/shop"
+            className="inline-block text-base sm:text-lg font-medium text-gray-800 underline underline-offset-4 decoration-2 hover:text-gray-600 transition"
+          >
+            Shop Now
+          </Link>
+        </div>
 
-export default Hero
+        {/* Image Section */}
+        <div className="flex justify-center">
+          <Image
+            src="/pic/Rocket.png"
+            alt="Rocket Sofa"
+            width={500}
+            height={500}
+            className="rounded-lg object-contain"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
